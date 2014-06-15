1ce9b792080bf2f7b1bdf7b51c0c6f5dbdfb12c5
caibinterry <terrytsaic@gmail.com>
2014-06-16 02:01:48 +0800

    修改个人信息

--- book/src/com/software3/servlet/UpdateUserServlet.java
+++ book/src/com/software3/servlet/UpdateUserServlet.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.software3.pojo.User;
+import com.software3.service.UserService;
+import com.software3.service.impl.UserServiceImpl;
+
 @WebServlet("/updateuser")
 public class UpdateUserServlet extends HttpServlet
 {
@@ -14,8 +18,25 @@ public class UpdateUserServlet extends HttpServlet
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	UserService us = new UserServiceImpl();
+	User user = null;
 	protected void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException{
-		response.getWriter().write("success");
+		user = new User();
+		user.setStudentid(request.getParameter("studentid"));
+		user.setName(request.getParameter("name"));
+		user.setCollege(request.getParameter("college"));
+		user.setClassandgrade(request.getParameter("classandgrade"));
+		user.setSex(request.getParameter("sex"));
+		user.setPhone(request.getParameter("phone"));
+		user.setWechat(request.getParameter("wechat"));
+		user.setWeibo(request.getParameter("weibo"));
+		user.setPersonerinfo(request.getParameter("personerinfo"));
+		user.setCredits(Integer.parseInt(request.getParameter("credits")));
+		user.setLevel(Integer.parseInt(request.getParameter("level")));
+		if(us.updateUser(user)){
+			response.getWriter().write("success");
+		}else{
+			response.getWriter().write("fail");
+		}
 	}
 }
